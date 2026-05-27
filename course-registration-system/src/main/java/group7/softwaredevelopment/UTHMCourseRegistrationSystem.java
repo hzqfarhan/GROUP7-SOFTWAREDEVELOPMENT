225c32eb514210d71b463af3c1c7990d6b2f0723
nrsyfeaa <sofeaamer02@gmail.com>
2026-05-27 22:17:25 +0800

    Update UTHMCourseRegistrationSystem.java

--- course-registration-system/src/main/java/group7/softwaredevelopment/UTHMCourseRegistrationSystem.java
+++ course-registration-system/src/main/java/group7/softwaredevelopment/UTHMCourseRegistrationSystem.java
@@ -1,7 +1,4 @@
 import java.sql.*;
-import java.util.Scanner;
-import io.github.cdimascio.dotenv.Dotenv;
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
